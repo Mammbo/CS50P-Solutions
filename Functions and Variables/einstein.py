955abeaa1b04e77859010143eab052de3a98ea7c
Mammbo <113815614+Mammbo@users.noreply.github.com>
2024-03-13 23:13:08 -0500

    deleted some old folders

--- Functions and Variables/einstein.py
+++
@@ -1,8 +0,0 @@
-light = 300000000
-
-def einstein(mass):
-    energy = mass * light ** 2
-    print(f"Energy: {energy}")
-
-mass =  int(input("What is the mass of your object in Kg's: "))
-einstein(mass)
